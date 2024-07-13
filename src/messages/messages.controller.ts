fb6ae3de11ee028927e2738ff45ee41349dcbab3
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-07-13 16:50:52 +0530

    feat: handle error when creating new message

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, NotFoundException } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -22,8 +22,10 @@ export class MessagesController {
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    return this.messagesService.findOne(id);
-  }
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (!message) throw new NotFoundException('Message Not Found');
 
+    return message;
+  }
 }
--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -2,24 +2,24 @@ import { readFile, writeFile } from 'fs/promises';
 
 export class MessagesRepository {
   async findOne(id: string) {
-    const contents = await readFile('message.json', 'utf-8');
+    const contents = await readFile('messages.json', 'utf-8');
     const messages = JSON.parse(contents);
 
     return messages[id];
   }
 
   async findAll() {
-    const contents = await readFile('message.json', 'utf-8');
+    const contents = await readFile('messages.json', 'utf-8');
     return JSON.parse(contents);
   }
 
   async create(content: string) {
-    const contents = await readFile('message.json', 'utf-8');
+    const contents = await readFile('messages.json', 'utf-8');
     const messages = JSON.parse(contents);
 
     const id = Math.floor(Math.random() * 999);
     messages[id] = { id, content };
 
-    await writeFile('message.json', JSON.stringify(messages));
+    await writeFile('messages.json', JSON.stringify(messages));
   }
 }
